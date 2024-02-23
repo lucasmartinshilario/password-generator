ffe29551c48663b578f8806d3fff0f92fbf9d4be
Lucas <lucas_mhilario@hotmail.com>
2024-02-23 09:27:23 -0300

    Add my local project to my GitHub repository

---
+++ src/components/modal/index.js
@@ -0,0 +1,57 @@
+import { Text, View, StyleSheet, TouchableOpacity, Pressable } from "react-native";
+
+export function ModalPassword() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.title}> Senha gerada</Text>
+
+                <Pressable style={styles.innerPassword}>
+                    <Text style={styles.text}>123123</Text>
+                </Pressable>
+
+
+                <View>
+                    
+                </View>
+
+
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'rgba(24, 24, 24, 0.6)',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    content: {
+        backgroundColor: '#fff',
+        width: '85%',
+        paddingTop: 24,
+        paddingBottom: 24,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+        marginBottom: 24
+    },
+    innerPassword: {
+        backgroundColor: '#0e0e0e',
+        width: '90%',
+        padding: 14,
+        borderRadius: 8
+    },
+    text: {
+        color: '#fff',
+        textAlign: 'center'
+    }
+})
